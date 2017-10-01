e79bb1b8a9e6859c37e047541aa5d888e17504a3
pkj <mepankaj@live.in>
2017-10-01 03:04:35 -0400

    Show user info into modal

--- fetch_user.php
+++ fetch_user.php
@@ -1,6 +1,10 @@
 <?php include "db_operations.php" ?>
 <?php
-
     $current_user = db_select("SELECT * FROM users WHERE user_id = $_POST('userid'));
     echo $current_user[0]['user_name'];
 ?>
+    <p>Bio: <?php echo $current_user[0]['user_bio']; ?></p>
+
+    <ul class="list-group z-depth-0">
+        <li class="list-group-item justify-content-between"><?php echo $current_user[0]['user_skills']; ?></li>
+    </ul>
--- partials/full-profile-modal.php
+++ partials/full-profile-modal.php
@@ -1,47 +1,41 @@
 <!-- Modal -->
-<div class="modal fade right" id="basicExample" tabindex="-1"
-role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-full-height modal-right" role="document">
-	<!--Content-->
-	<div class="modal-content">
-		<!--Header-->
-		<div class="modal-header">
-			<h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
-			<button type="button" class="close" data-dismiss="modal"
-			aria-label="Close">
-			<span aria-hidden="true">�</span>
-		</button>
-	</div>
-	<!--Body-->
-	<div class="modal-body">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-			Sapiente magnam, sunt, ea dolore eum quod. Minima fugiat enim aut
-			soluta est reprehenderit reiciendis quos, qui, amet possimus
-			laborum assumenda voluptate.</p>
+<div class="modal fade right" id="basicExample" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-full-height modal-right" role="document">
+		<div class="modal-content"><!--Header-->
+			<div class="modal-header">
+				<h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">�</span>
+				</button>
+			</div>
+		</div><!--Body-->
+		<div class="modal-body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Sapiente magnam, sunt, ea dolore eum quod. Minima fugiat enim aut
+				soluta est reprehenderit reiciendis quos, qui, amet possimus
+				laborum assumenda voluptate.</p>
 
-			<ul class="list-group z-depth-0">
-				<li class="list-group-item justify-content-between">Cras justo
-					odio <span class="badge badge-primary badge-pill">14</span>
-				</li>
-				<li class="list-group-item justify-content-between">Dapibus ac
-					facilisis in <span class="badge badge-primary badge-pill">2</span>
-				</li>
-				<li class="list-group-item justify-content-between">Morbi leo
-					risus <span class="badge badge-primary badge-pill">1</span>
-				</li>
-				<li class="list-group-item justify-content-between">Cras justo
-					odio <span class="badge badge-primary badge-pill">14</span>
-				</li>
-				<li class="list-group-item justify-content-between">Dapibus ac
-					facilisis in <span class="badge badge-primary badge-pill">2</span>
-				</li>
-				<li class="list-group-item justify-content-between">Morbi leo
-					risus <span class="badge badge-primary badge-pill">1</span>
-				</li>
-			</ul>
-
-		</div>
-		<!--Footer-->
+				<ul class="list-group z-depth-0">
+					<li class="list-group-item justify-content-between">Cras justo
+						odio <span class="badge badge-primary badge-pill">14</span>
+					</li>
+					<li class="list-group-item justify-content-between">Dapibus ac
+						facilisis in <span class="badge badge-primary badge-pill">2</span>
+					</li>
+					<li class="list-group-item justify-content-between">Morbi leo
+						risus <span class="badge badge-primary badge-pill">1</span>
+					</li>
+					<li class="list-group-item justify-content-between">Cras justo
+						odio <span class="badge badge-primary badge-pill">14</span>
+					</li>
+					<li class="list-group-item justify-content-between">Dapibus ac
+						facilisis in <span class="badge badge-primary badge-pill">2</span>
+					</li>
+					<li class="list-group-item justify-content-between">Morbi leo
+						risus <span class="badge badge-primary badge-pill">1</span>
+					</li>
+				</ul>
+		</div><!--Footer-->
 		<div class="modal-footer text-center">
 			<a href ="https://twitter.com/"><i class="fa fa-twitter circle text-success"></i></a>
 			<a href ="https://www.facebook.com/"><i class="fa fa-facebook circle text-success"></i></a>
@@ -49,13 +43,9 @@ role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 			<a href ="https://dribbble.com/"><i class="fa fa-dribbble circle text-success"></i></a>
 			<a href ="https://www.pinterest.com/"><i class="fa fa-pinterest circle text-success"></i></a>
 			<a href ="https://www.youtube.com/"><i class="fa fa-youtube circle text-success"></i></a>
-			<button type="button pull-right" class="close" data-dismiss="modal"
-			aria-label="Close">
-			<span aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i></span>
-		</button>
-	</div>
-</div>
-<!--/.Content-->
-</div>
-</div>
-<!-- Modal -->
+			<button type="button pull-right" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i></span>
+			</button>
+		</div>
+	</div><!--/.Content-->
+</div><!-- Modal -->
