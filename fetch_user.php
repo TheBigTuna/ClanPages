051aedbde5284f9f059f87304298c391d3498242
pkj <mepankaj@live.in>
2017-10-01 03:38:42 -0400

    changes

--- fetch_user.php
+++ fetch_user.php
@@ -1,8 +1,8 @@
 <?php include "db_operations.php" ?>
 <?php
     $current_user = db_select("SELECT * FROM users WHERE user_id ='" . $_POST['userid'] . "'");
-    echo $current_user[0]['user_name'];
 ?>
+    <h4 class="mx-auto mb-2"><?php echo $current_user[0]['user_name']; ?></h4>
     <ul class="list-group z-depth-0">
         <li class="list-group-item justify-content-between">Bio: <?php echo $current_user[0]['user_bio']; ?></li>
         <li class="list-group-item justify-content-between">Location: <?php echo $current_user[0]['user_location']; ?></li>
