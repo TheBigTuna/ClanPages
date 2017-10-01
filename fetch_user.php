12c5234de95e8090c86990359b49692e803a2253
Nick Holbrook <ndh175@gmail.com>
2017-10-01 03:20:47 -0400

    Modal Upgrades

--- fetch_user.php
+++ fetch_user.php
@@ -1,14 +1,11 @@
 <?php include "db_operations.php" ?>
 <?php
-<<<<<<< HEAD
-    $current_user = db_select("SELECT * FROM users WHERE user_id = $_POST('userid'));
-=======
     $current_user = db_select("SELECT * FROM users WHERE user_id ='" . $_POST['userid'] . "'");
->>>>>>> 6949e117218ef6e10872818b7d458342f412cf56
     echo $current_user[0]['user_name'];
 ?>
-    <p>Bio: <?php echo $current_user[0]['user_bio']; ?></p>
-
     <ul class="list-group z-depth-0">
-        <li class="list-group-item justify-content-between"><?php echo $current_user[0]['user_skills']; ?></li>
+        <li class="list-group-item justify-content-between">Bio: <?php echo $current_user[0]['user_bio']; ?></li>
+        <li class="list-group-item justify-content-between">Location: <?php echo $current_user[0]['user_location']; ?></li>
+        <li class="list-group-item justify-content-between">Major: <?php echo $current_user[0]['user_major']; ?></li>
+        <li class="list-group-item justify-content-between">Skills: <?php echo $current_user[0]['user_skills']; ?></li>
     </ul>
