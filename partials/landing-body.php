babd3841c06548ab05e68d2303c2f8b6041ea0c7
pkj <mepankaj@live.in>
2017-09-30 23:20:38 -0400

    Landing v1

--- partials/landing-body.php
+++ partials/landing-body.php
@@ -1,1 +1,19 @@
-<h1>hello</h1>
+<div class="container jumbotron text-center mt-5">
+	<div class="">
+		<h1 class="display-3 mt-3 mb-4" style="font-size:3.65rem">Connect your community with ClanPages.</h1>
+        <form action="/create-clan.php">
+            <button type="submit" class="btn btn-success btn-lg">Create Your ClanPage</button>
+        </form>
+		<form method="" action="" class="mt-5">
+			<div class="row justify-content-md-center">
+				<div class="col-sm-6">
+					<div class="input-group">
+						<span class="input-group-addon"> <i class="fa fa-search"></i>
+						</span> <input type="text" class="form-control"
+							placeholder="Search your clan...">
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
